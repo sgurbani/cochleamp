53b41146055f186c90bad6a01353a55f6baa40b1
ywlke287@gmail.com <ywlke287@gmail.com@3cb66886-0bef-b43e-1472-57f9efbd4e36>
2011-11-23 21:25:07 +0000

    updated parameters

--- trunk/utils/make_obstacles.m
+++ trunk/utils/make_obstacles.m
@@ -1,4 +1,6 @@
-for theta=0:pi/180:2*pi
+diary on
+for theta=0:pi/180:4*pi
 	r=9.43*2.618/(theta+4.1);
-	fprintf('%f %f 0.1\n',5+r*cos(pi/2-theta),5+r*sin(pi/2-theta));
+	fprintf('%f %f 0.1\n',5+r*cos(pi/2-theta),r*sin(pi/2-theta));
 end
+diary off
