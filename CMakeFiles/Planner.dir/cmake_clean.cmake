21f29f07959efc15cc9d25ccf1cf08b81ffed765
ywlke287@gmail.com <ywlke287@gmail.com@3cb66886-0bef-b43e-1472-57f9efbd4e36>
2011-11-23 17:53:30 +0000

    Initial add.

---
+++ CMakeFiles/Planner.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Planner.dir/src/ManipPlanner.o"
+  "CMakeFiles/Planner.dir/src/Graphics.o"
+  "CMakeFiles/Planner.dir/src/ManipSimulator.o"
+  "bin/Planner.pdb"
+  "bin/Planner"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
