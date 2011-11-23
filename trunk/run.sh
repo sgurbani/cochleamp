3526dc6371391b87a1d6632d18c5341e824d4bf8
ywlke287@gmail.com <ywlke287@gmail.com@3cb66886-0bef-b43e-1472-57f9efbd4e36>
2011-11-23 23:59:35 +0000

    well ok I guess I forgot to commit these...

---
+++ trunk/run.sh
@@ -0,0 +1,1 @@
+bin/Planner bin/obstacles.txt 8 1.5
--- trunk/utils/make_obstacles.m
+++ trunk/utils/make_obstacles.m
@@ -1,6 +1,7 @@
 diary on
+fprintf('721\n');
 for theta=0:pi/180:4*pi
 	r=9.43*2.618/(theta+4.1);
-	fprintf('%f %f 0.1\n',5+r*cos(pi/2-theta),r*sin(pi/2-theta));
+	fprintf('%f %f 0.1\n',5+r*cos(pi/2-theta),-1+r*sin(pi/2-theta));
 end
 diary off
