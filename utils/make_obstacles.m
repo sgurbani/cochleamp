9ed093bc1c01a995eda5ba8f56e74bde31376ffc
ywlke287@gmail.com <ywlke287@gmail.com@3cb66886-0bef-b43e-1472-57f9efbd4e36>
2011-12-03 02:56:39 +0000

    Updated obstacles script to make longer cochlea outer wall

--- utils/make_obstacles.m
+++ utils/make_obstacles.m
@@ -1,6 +1,6 @@
 diary on
-fprintf('721\n');
-for theta=0:pi/180:4*pi
+fprintf('738\n');
+for theta=-0.3:pi/180:4*pi
 	r=9.43*2.618/(theta+4.1);
 	fprintf('%f %f 0.1\n',5+r*cos(pi/2-theta),-1+r*sin(pi/2-theta));
 end
